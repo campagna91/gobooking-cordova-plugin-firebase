1795ebdeebbb3f511efd5e010c870a53da08d787
SimoneCampagna <lavoroeclienti@gmail.com>
2018-09-21 19:11:30 +0200

    CQI: enhace plugin print

--- scripts/after_plugin_add.js
+++ scripts/after_plugin_add.js
@@ -10,6 +10,10 @@ module.exports = function(context) {
     // Modify the Gradle build file to add a task that will upload the debug symbols
     // at build time.
     if (platforms.indexOf("android") !== -1) {
+      console.log("* * * * * * * * * * * * * * * * * * * * *");
+      console.log("* * * * A  N  D  R  O  I  D * * * * * * *");
+      console.log("* * * * *  H  O  O  K  S  * * * * * * * *");
+      console.log("* * * * * * * * * * * * * * * * * * * * *");
         androidHelper.restoreRootBuildGradle();
         androidHelper.modifyRootBuildGradle();
     }
@@ -17,6 +21,10 @@ module.exports = function(context) {
     // Add a build phase which runs a shell script that executes the Crashlytics
     // run command line tool which uploads the debug symbols at build time.
     if (platforms.indexOf("ios") !== -1) {
+      console.log("* * * * * * * * * * * * * * * * * * * * *");
+      console.log("* * * * *  *  I  O  S  * * * * * * * * *");
+      console.log("* * * * *  H  O  O  K  S  * * * * * * * *");
+      console.log("* * * * * * * * * * * * * * * * * * * * *");
         var xcodeProjectPath = utilities.getXcodeProjectPath(context);
         iosHelper.removeShellScriptBuildPhase(context, xcodeProjectPath);
         iosHelper.addShellScriptBuildPhase(context, xcodeProjectPath);
