d72bca015c6575e6c05409a1302f88238ce4ac63
SimoneCampagna <lavoroeclienti@gmail.com>
2018-09-21 18:50:53 +0200

    CFG: update addDependencies() top level function in android-helper.js library

--- scripts/lib/android-helper.js
+++ scripts/lib/android-helper.js
@@ -21,13 +21,21 @@ function addDependencies(buildGradle) {
   // find the known line to match
   var match = buildGradle.match(/^(\s*)classpath 'com.android.tools.build(.*)/m);
   var whitespace = match[1];
-  
+
   // modify the line to add the necessary dependencies
   var googlePlayDependency = whitespace + 'classpath \'com.google.gms:google-services:+\' // google-services dependency from cordova-plugin-firebase';
   var fabricDependency = whitespace + 'classpath \'io.fabric.tools:gradle:1.25.4\' // fabric dependency from cordova-plugin-firebase'
   var androidBuildToolDependency = whitespace + 'classpath \'com.android.tools.build:gradle:3.0.1\' // google-services dependency from cordova-plugin-firebase';
-  var modifiedLine = match[0] + '\n' + googlePlayDependency + '\n' + fabricDependency + '\n' + androidBuildToolDependency;
-  
+  var modifiedLine = '\n' + googlePlayDependency + '\n' + fabricDependency + '\n' + androidBuildToolDependency;
+
+  // Replace current 
+  // buildGradle.replace("classpath 'com.android.tools.build:gradle:2.2.3'", "");
+
+  // Update upper apply classpath
+  buildGradle.replace("debugCompile(project(path: \"CordovaLib\", configuration: \"debug\"))", "");
+  buildGradle.replace("releaseCompile(project(path: \"CordovaLib\", configuration: \"release\"))", "");
+
+
   // modify the actual line
   return buildGradle.replace(/^(\s*)classpath 'com.android.tools.build(.*)/m, modifiedLine);
 }
@@ -69,7 +77,7 @@ module.exports = {
 
     // Add Google Play Services Dependency
     buildGradle = addDependencies(buildGradle);
-  
+
     // Add Google's Maven Repo
     buildGradle = addRepos(buildGradle);
 
@@ -86,7 +94,7 @@ module.exports = {
 
     // remove any lines we added
     buildGradle = buildGradle.replace(/(?:^|\r?\n)(.*)cordova-plugin-firebase*?(?=$|\r?\n)/g, '');
-  
+
     writeRootBuildGradle(buildGradle);
   }
 };
